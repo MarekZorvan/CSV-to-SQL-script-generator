d9a7da65dd2a79effedd46441499871e810bc0c5
Marek Zorvan <marek.zorvan@siemens.com>
2020-02-28 16:16:47 +0100

    first version untested

--- CSV-Data_to_SQLscript/MainWindow.xaml.cs
+++ CSV-Data_to_SQLscript/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +14,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using ScriptGenerator;
 
 namespace CSV_Data_to_SQLscript
 {
@@ -23,6 +26,44 @@ namespace CSV_Data_to_SQLscript
         public MainWindow()
         {
             InitializeComponent();
+
+            this.PathLabel.Content = "Please select your CSV file first...";
+            this.CreateScriptButton.IsEnabled = false;
         }
+
+        private void CreateScriptButton_Click(object sender, RoutedEventArgs e)
+        {
+            SaveFileDialog dialog = new SaveFileDialog()
+            {
+                Filter = "Text Files(*.csv)|*"
+            };
+
+            if (dialog.ShowDialog() == true)
+            {
+                Generator scriptGen = new Generator(); 
+                string fileData = scriptGen.MakeScript(this.PathLabel.Content.ToString());
+                File.WriteAllText(dialog.FileName, fileData, Encoding.UTF8);
+            }
+        }
+
+        private void SelectPathButton_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog dialog = new OpenFileDialog()
+            {
+                // Filter = "Text Files(*.csv)|*"
+                Filter = "csv files (*.csv)|*.csv|All files (*.*)|*.*"
+            };
+
+            if (dialog.ShowDialog() == true)
+            {
+                this.PathLabel.Content = File.ReadAllText(dialog.FileName);
+                this.CreateScriptButton.IsEnabled = true;
+            }
+        }
+
+        //private void Button_Click(object sender, RoutedEventArgs e)
+        //{
+
+        //}
     }
 }
--- ScriptGenerator/Generator.cs
+++ ScriptGenerator/Generator.cs
@@ -1,8 +1,76 @@
-﻿using System;
+﻿using CsvHelper;
+using System;
+using System.Collections.Generic;
+using System.IO;
 
 namespace ScriptGenerator
 {
     public class Generator
     {
+        private static string permittedExtension = ".csv";
+        public Generator()
+        {
+
+        }
+
+        public string MakeScript(string filePath)
+        {
+            string script = "";
+
+            if (!string.IsNullOrEmpty(filePath) && filePath.Substring(filePath.Length - 4).Contains(permittedExtension))
+            {
+                // The file extension is valid
+
+                // column names from headers, are expected to be in the 1st row of CSV file
+                string[] firstRowValues;
+
+                using (var file = File.Open(filePath, FileMode.Open, FileAccess.Read, FileShare.ReadWrite))
+                {
+                    var reader = new StreamReader(file);
+
+                    var parser = new CsvParser(reader);
+
+                    // save headers
+                    firstRowValues = parser.Read();
+
+                    string commandStart = "INSERT INTO table(";
+
+                    for (int i = 0; i < firstRowValues.Length - 1; i++)
+                    {
+                        commandStart += firstRowValues[i] + ", ";
+                    }
+
+                    commandStart += firstRowValues[firstRowValues.Length - 1] + ")\n";
+                    commandStart += "VALUES\n";
+
+                    // values of 1 row
+                    string[] row = parser.Read();
+
+                    while (row != null)
+                    {
+                        //TODO: ADD INSERT INTO SQL COMMAND TO variable script which will be returned
+
+                        // INSERT INTO table(column1, column2, …)
+                        // VALUES
+                        // (value1, value2, …);
+
+                        string command = commandStart + "(";
+
+                        for (int i = 0; i < row.Length - 1; i++)
+                        {
+                            command += row[i] + ", ";
+                        }
+
+                        command += row[row.Length - 1] + ")\n;";
+
+                        script += command;
+
+                        row = parser.Read();
+                    }
+                }
+            }
+
+            return script;
+        }
     }
 }
